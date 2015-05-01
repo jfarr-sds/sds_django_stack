bbecc6434d19692ca8f5d8d78e8281f15c4ebd0d
Jeff Farr <jfarr-sds@Jeffs-MacBook-Pro-2.local>
2015-05-01 17:34:01 -0400

    trivial nginx install done

---
+++ chef-repo/cookbooks/sds_django_cookbook/metadata.rb
@@ -0,0 +1,10 @@
+name             'sds_django_cookbook'
+maintainer       'Sentient Decision Science'
+maintainer_email 'jfarr@sentientdecisionscience.com'
+license          'All rights reserved'
+description      'Installs/Configures a django stack'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "git"
+depends "nginx"
---
+++ chef-repo/cookbooks/sds_django_cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: sds_django_cookbook
+# Recipe:: default
+#
+# Copyright 2015, Sentient Decision Science
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "nginx"
+include_recipe "git"
